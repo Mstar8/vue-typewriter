6dd9c01ae5bc977970f402ebdfb38e38ba172fbe
Mstar <1319335fu@gmail.com>
2021-04-15 00:51:37 +0800

    精简代码

--- typewriter.vue
+++ typewriter.vue
@@ -11,7 +11,10 @@ export default {
   name: "typerwriter",
   props: {
     text: String,
-    texts: Array,
+    texts: {
+      type: Array,
+      default: new Array(),
+    },
     // 打印速度
     wspeed: {
       type: Number,
@@ -25,6 +28,7 @@ export default {
   },
   data() {
     return {
+      loaclTexts: this.texts,
       outputText: "",
       // 当前打印字符串Index
       currentTextCount: 1,
@@ -56,111 +60,71 @@ export default {
       if (!this.argumentJudge()) {
         return;
       }
-      // 先判断打印字符串还是字符数组
-      if (this.text == null && this.texts != null) {
-        // 打印字符数组
-        this.timer = setInterval(this.writerArray, this.wspeed);
-      } else if (this.text != null && this.texts == null) {
-        // 打印字符
-        this.timer = setInterval(this.writerString, this.wspeed);
-      } else if (this.texts != null && this.text != null) {
-        // 两者都有打印字符数组
-        this.timer = setInterval(this.writerArray, this.wspeed);
+      // 只设置了text，将text放入loaclTexts
+      if (this.loaclTexts.length == 0) {
+        this.loaclTexts.push(this.text);
       }
+      // 开启打印
+      this.timer = setInterval(this.forwardWriter, this.wspeed);
     },
-    writerArray() {
-      if (this.currentTextArrayIndex >= this.texts.length) {
-        // 往回打印的情况
+    forwardWriter() {
+      // 全部打印完判断
+      if (this.currentTextArrayIndex >= this.loaclTexts.length) {
+        // 往回打印的情况，重置参数
         if (this.isBack) {
           this.currentTextArrayIndex = 0;
           this.currentTextCount = 1;
         } else {
+          // 不往回打印, 取消返回
+          clearInterval(this.timer);
           return;
         }
       }
       if (this.forwadFlag) {
+        // 往前打印
         if (
-          // 打印
-          this.currentTextCount <= this.texts[this.currentTextArrayIndex].length
+          this.currentTextCount <=
+          this.loaclTexts[this.currentTextArrayIndex].length
         ) {
-          this.outputText = this.texts[this.currentTextArrayIndex].substring(
-            0,
-            this.currentTextCount
-          );
+          this.outputText = this.loaclTexts[
+            this.currentTextArrayIndex
+          ].substring(0, this.currentTextCount);
           this.currentTextCount++;
         } else {
-          // 当前字符串打印完，指向下一个并把索引归0
-          // 判断是否需要向后消除打印
+          // 当前字符串打印完，指向下一个并把索引归1
+          // 判断是否需要向后打印
           if (this.isBack) {
             this.forwadFlag = false;
           } else {
             this.currentTextArrayIndex++;
-            this.currentTextCount = 0;
+            this.currentTextCount = 1;
           }
         }
       } else {
         // 向后打印
         if (this.isBack) {
           clearInterval(this.timer);
-          // 向后打印2倍速度
-          this.timer = setInterval(this.backWriterArray, this.wspeed / 2.5);
+          // 向后打印2.5倍速度
+          this.timer = setInterval(this.backWriter, this.wspeed / 2.5);
         }
       }
     },
-    writerString() {
-      // 先判断是否向前打印
-      if (this.forwadFlag) {
-        // 向前打印
-        if (this.currentTextCount <= this.text.length) {
-          this.outputText = this.text.substring(0, this.currentTextCount);
-          this.currentTextCount++;
-        } else {
-          // 打印完判断是否需要向后打印
-          if (this.isBack) {
-            this.forwadFlag = false;
-          } else {
-            // 不需要取消定时器
-            clearInterval(this.timer);
-          }
-        }
-      } else {
-        // 向后打印
-        if (this.isBack) {
-          clearInterval(this.timer);
-          // 向后打印2倍速度
-          this.timer = setInterval(this.backWriterString, this.wspeed / 2.5);
-        }
-      }
-    },
-    backWriterString() {
+    backWriter() {
       if (this.currentTextCount >= 0) {
-        this.outputText = this.text.substring(0, this.currentTextCount);
-        this.currentTextCount--;
-      } else {
-        // 转回去从第一个开始打印
-        this.currentTextCount = 1;
-        clearInterval(this.timer);
-        // 打印完转为向前打印
-        this.forwadFlag = true;
-        this.timer = setInterval(this.writerString, this.wspeed);
-      }
-    },
-    backWriterArray() {
-      if (this.currentTextCount >= 0) {
-        this.outputText = this.texts[this.currentTextArrayIndex].substring(
+        this.outputText = this.loaclTexts[this.currentTextArrayIndex].substring(
           0,
           this.currentTextCount
         );
         this.currentTextCount--;
       } else {
         // 当前字符串打印完
-        // 指针自增打印下一个
+        // 指针自增打印下一个字符串
         this.currentTextArrayIndex++;
         this.forwadFlag = true;
         this.currentTextCount = 1;
         // 取消当前timer
         clearInterval(this.timer);
-        this.timer = setInterval(this.writerArray, this.wspeed);
+        this.timer = setInterval(this.forwardWriter, this.wspeed);
       }
     },
     argumentJudge() {
